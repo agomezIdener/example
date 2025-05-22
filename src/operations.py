ac7c8c348ea3761b8be908a6cf5e8a422b24a5b0
agomezIdener <112472030+agomezIdener@users.noreply.github.com>
2025-05-22 13:15:20 +0200

    Other operation

--- src/operations.py
+++ src/operations.py
@@ -4,6 +4,8 @@ class Operations:
         self.b = 4
     def display(self):
         print(f"a = {self.a}, b = {self.b}")
+    def subtract(self):
+        return self.a - self.b
 
 print("Operations module loaded")
 Operations().display()
