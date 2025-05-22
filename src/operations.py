070d1f1a46c37c34e8323ae2b46b97d171c30eeb
agomezIdener <112472030+agomezIdener@users.noreply.github.com>
2025-05-22 13:04:57 +0200

    Create operation, first commit

---
+++ src/operations.py
@@ -0,0 +1,9 @@
+class Operations:
+    def __init__(self):
+        self.a = 5
+        self.b = 4
+    def display(self):
+        print(f"a = {self.a}, b = {self.b}")
+
+print("Operations module loaded")
+Operations().display()
